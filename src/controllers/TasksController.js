05a547b6bbfbfd93a2c3cf014f3f192e4cd0dad7
Thiago de Freitas <thiagodefreitas02@gmail.com>
2023-04-03 19:07:45 -0300

    Criado rota update

--- src/controllers/TasksController.js
+++ src/controllers/TasksController.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable prefer-const */
 import { get } from 'lodash';
 import Tasks from '../models/Tasks';
 
@@ -8,7 +8,7 @@ class TasksController {
       const tasks = await Tasks.findAll();
       return res.json(tasks);
     } catch (e) {
-      const eToMap = get(e, 'errors', { message: 'Unkown error' });
+      const eToMap = get(e, 'errors', [{ message: 'Unkown error' }]);
       return res.status(400).json({
         errors: eToMap.map((err) => err.message),
       });
@@ -17,10 +17,18 @@ class TasksController {
 
   async store(req, res) {
     try {
-      const task = await Tasks.create(req.body);
+      const { name, status, delivery_date } = req.body;
+
+      if (status || delivery_date) {
+        return res.status(400).json({
+          errors: ['Status or delivery_date enableds in update route'],
+        });
+      }
+
+      const task = await Tasks.create(name);
       return res.json(task);
     } catch (e) {
-      const eToMap = get(e, 'errors', { message: 'Unkown error' });
+      const eToMap = get(e, 'errors', [{ message: 'Unkown error' }]);
       return res.status(400).json({
         errors: eToMap.map((err) => err.message),
       });
@@ -43,7 +51,97 @@ class TasksController {
       }
       return res.json(task);
     } catch (e) {
-      const eToMap = get(e, 'errors', { message: 'Unkown error' });
+      const eToMap = get(e, 'errors', [{ message: 'Unkown error' }]);
+      return res.status(400).json({
+        errors: eToMap.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing ID parameter'],
+        });
+      }
+      const task = await Tasks.findByPk(id);
+      if (!task) {
+        return res.status(400).json({
+          errors: ['Task not exists'],
+        });
+      }
+
+      const toEdit = {};
+      const { name, status, delivery_date } = req.body;
+
+      if (name) toEdit.name = name;
+
+      if (status && task.status === 2) {
+        return res.status(400).json({
+          errors: ['Task was finished'],
+        });
+      }
+
+      if (status != null) {
+        if (status === 0 || status === 1 || status === 2) {
+          toEdit.status = status;
+        } else {
+          return res.status(400).json({
+            errors: ['Status can be 0 (pending), 1 (progessing), 2 (finished)'],
+          });
+        }
+      }
+
+      if (delivery_date) {
+        const { year, month, day } = delivery_date;
+
+        // Check if exists
+        if (!year || !month || !day) {
+          return res.status(400).json({
+            errors: ['delivery_date can be an dict with year, month and day'],
+          });
+        }
+
+        // Check if are valids
+        if (!Number.isInteger(year)) {
+          return res.status(400).json({
+            errors: ['delivery_date\'s year not valid'],
+          });
+        }
+
+        if (!Number.isInteger(month) || month < 1 || month > 12) {
+          return res.status(400).json({
+            errors: ['delivery_date\'s month not valid'],
+          });
+        }
+
+        if (!Number.isInteger(day) || day < 1 || day > 31) {
+          return res.status(400).json({
+            errors: ['delivery_date\'s day not valid'],
+          });
+        }
+        const aux_delivery_date = new Date(year, month - 1, day);
+
+        let now = new Date();
+
+        now.setDate(now.getDate() - 1);
+
+        if (aux_delivery_date <= now) {
+          return res.status(400).json({
+            errors: ['Insert a valid dalivery_date'],
+          });
+        }
+
+        toEdit.delivery_date = `${aux_delivery_date.getFullYear()}-${aux_delivery_date.getMonth() + 1}-${aux_delivery_date.getDate()}`;
+      }
+
+      const updateTask = await task.update(toEdit);
+      return res.json(updateTask);
+    } catch (e) {
+      console.log(e);
+      const eToMap = get(e, 'errors', [{ message: 'Unkown error' }]);
       return res.status(400).json({
         errors: eToMap.map((err) => err.message),
       });
@@ -51,30 +149,6 @@ class TasksController {
   }
 
   /*
-  async update(req, res) {
-    try {
-      const { id } = req.params;
-      if (!id) {
-        return res.status(400).json({
-          errors: ['Faltando id'],
-        });
-      }
-      const aluno = await Aluno.findByPk(id);
-      if (!aluno) {
-        return res.status(400).json({
-          errors: ['Aluno não existe'],
-        });
-      }
-
-      const alunoAtualizado = await aluno.update(req.body);
-      return res.json(alunoAtualizado);
-    } catch (e) {
-      return res.status(400).json({
-        errors: e.errors.map((err) => err.message),
-      });
-    }
-  }
-
   async delete(req, res) {
     try {
       const { id } = req.params;
--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -5,7 +5,7 @@ const router = new Router();
 
 router.get('/', tasksController.index);
 router.post('/', tasksController.store);
-// router.put('/:id', tasksController.update);
+router.put('/:id', tasksController.update);
 router.get('/:id', tasksController.show);
 // router.delete('/:id', tasksController.delete);
 export default router;
